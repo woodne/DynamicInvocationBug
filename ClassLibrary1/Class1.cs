93a05cc70edae936eea26fe09b8aab7e7d8d4ea2
Nick Wood <Nick_Wood@ultimatesoftware.com>
2018-06-22 11:40:50 -0400

    initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public async Task<bool> MyMethod(Guid someId, Guid anotherId, Guid oneMoreID, string something, string whatever, int num = 0, dynamic foo = default(dynamic))
+        {
+            Console.WriteLine($"MyMethod: {PrintByteArray(someId.ToByteArray())}"); 
+            Console.WriteLine($"MyMethod Second Guid: {anotherId.ToString()}");
+            
+            return (await this.MyOtherMethod(someId, anotherId, oneMoreID, something, whatever, num, foo) == 1);
+        }
+
+        public Task<int> MyOtherMethod(Guid thatId, Guid anotherId, Guid oneMoreId, string something, string whatever, int num, dynamic foo = default(dynamic))
+        {
+            Console.WriteLine($"MyOtherMethod: {PrintByteArray(thatId.ToByteArray())}");
+            Console.WriteLine($"MyOtherMethod Second Guid: {anotherId.ToString()}");
+
+            return Task.FromResult(1);
+        }
+
+        private static string PrintByteArray(byte[] ba)
+        {
+            var sb = new StringBuilder(ba.Length * 2);
+            foreach (var b in ba)
+            {
+                sb.AppendFormat("{0:x2}", b);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            
+            Console.WriteLine($"Main: {id.ToByteArray().ToString()}");
+            var myObject = new Class1();
+            
+            var myClass = myObject.MyMethod(id, Guid.NewGuid(), Guid.Empty, "something", "something2", 0, new {}).Result;
+//            Console.WriteLine("Hello World!");
+        }
+    }
+}
